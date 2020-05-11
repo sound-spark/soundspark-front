15cb4e596c33e19041c63bd7b5decf4f0e4a2fef
Michał Kostyk <michasio25@gmail.com>
2020-05-11 10:23:18 +0200

    force heroku ssl

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-//Install express server
+const forceSecure = require("force-secure-express");
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+app.use(forceSecure([
+    "soundspark.herokuapp.com"
+]));
+
 app.use(express.static(__dirname + '/dist/soundspark-front'));
 
 app.get('/*', function(req,res) {
@@ -12,5 +15,4 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/soundspark-front/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
