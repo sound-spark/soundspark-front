774ce4508026c969bf0dfab5f2674fb71b1140c8
Michał Kostyk <michasio25@gmail.com>
2020-05-30 23:30:18 +0200

    delete interceptor and loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AudioComponent } from './component/audio/audio.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { MyLoaderComponent } from './component/my-loader/my-loader.component';
-import {LoaderService} from './service/loader/loader.service';
-import {LoaderInterceptor} from './interceptor/loader-interceptor.service';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AudioComponent} from './component/audio/audio.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AudioComponent,
-    MyLoaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    LoaderService,
-    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AudioComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
